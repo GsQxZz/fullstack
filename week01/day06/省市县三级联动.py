abb5245f941a2f2bb2aacdd84c7da3448c2b42d9
Neil <gsqxzfd@gmail.com>
2020-07-15 18:04:45 +0800

    高大上版

--- week01/day06/省市县三级联动.py
+++ week01/day06/省市县三级联动.py
@@ -1,23 +1,118 @@
 # __author: Richie
 # date:  2020/7/14
 
-city_dict = {
+menu = {
     '北京': {
-        '海淀区': ['西二旗', '中关村'],
-        '朝阳区': ['大望路', '国贸'],
-        '昌平区': ['天通苑', '回龙观']
+        '朝阳': {
+            '国贸': {
+                'CICC': {},
+                'HP': {},
+                '渣打银行': {},
+                'CCTV': {}
+            },
+            '望京': {
+                '陌陌': {},
+                '奔驰': {},
+                '360': {}
+            },
+            '三里屯': {
+                '优衣库': {},
+                'apple': {}
+            }
+        },
+        '昌平': {
+            '沙河': {
+                '老男孩': {},
+                '阿泰包子': {}
+            },
+            '天通苑': {
+                '链家': {},
+                '我爱我家': {}
+            },
+            '回龙观': {}
+        },
+        '海淀': {
+            '五道口': {
+                '谷歌': {},
+                '网易': {},
+                'Sohu': {},
+                'Sogo': {},
+                '快手': {}
+            },
+            '中关村': {
+                'youku': {},
+                'Iqiyi': {},
+                '汽车之家': {},
+                '新东方': {},
+                'QQ': {}
+            }
+        },
     },
-    '山西': {
-        '太原': ['迎泽公园', '小店'],
-        '忻州': ['定襄', '原平'],
-        '晋中': ['乔家大院', '平遥古城']
+    '上海': {
+            '浦东': {
+                '陆家嘴': {
+                    'CICC': {},
+                    '高盛': {},
+                    '摩根': {}
+                },
+                '外滩': {}
+            },
+            '闵行': {},
+            '静安': {}
     },
-    '河北': {
-        '石家庄': ['石家庄11', '石家庄22'],
-        '保定': ['保定11', '保定22'],
-        '承德': ['兴隆', '兴隆22']
+    '山东': {
+        '济南': {},
+        '德州': {
+            '乐陵': {
+                '丁乌镇': {},
+                '城区': {}
+            },
+            '平原': {}
+        },
+        '青岛': {}
     }
-
 }
 
-print('请选择省份:')
+back_flag = False
+exit_flag = False
+
+while not back_flag and not exit_flag:
+    for key in menu:
+        print(key)
+    choice = input("1>>:").strip()
+    if choice.lower() == 'q':
+        exit_flag = True
+    if choice in menu:
+        while not back_flag and not exit_flag:
+            for key2 in menu[choice]:
+                print(key2)
+            choice2 = input("2>>:").strip()
+            if choice2.lower() == 'b':
+                back_flag = True
+            if choice2.lower() == 'q':
+                exit_flag = True
+            if choice2 in menu[choice]:
+                while not back_flag and not exit_flag:
+                    for key3 in menu[choice][choice2]:
+                        print(key3)
+                    choice3 = input("3>>").strip()
+                    if choice3.lower() == 'b':
+                        back_flag = True
+                    if choice3.lower() == 'q':
+                        exit_flag = True
+                    if choice3 in menu[choice][choice2]:
+                        while not back_flag and not exit_flag:
+                            for key4 in menu[choice][choice2][choice3]:
+                                print(key4)
+                            choice4 = input('4>>').strip()
+                            print('last level')
+                            if choice4.lower() == 'b':
+                                back_flag = True
+                            if choice4.lower() == 'q':
+                                exit_flag = True
+                        else:
+                            back_flag = False
+                else:
+                    back_flag = False
+        else:
+            back_flag = False
